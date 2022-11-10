7db5ce88396ada2ca21d543116c3c8c7c22033c3
umutcrt <98687913+umutcrt@users.noreply.github.com>
2022-11-10 23:07:14 +0300

    Initial Commit

---
+++ movieLand/movieLandApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieLandApp.swift
+//  movieLand
+//
+//  Created by Umut Cörüt on 10.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct movieLandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
